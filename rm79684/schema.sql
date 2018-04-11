c8df3f0a72efdb6f641b14e4e539c93be3003343
Fernando Zendron <fernandozendron@Fernandos-MacBook-Pro.local>
2018-04-10 23:22:58 -0300

    First commit

---
+++ rm79684/schema.sql
@@ -0,0 +1,10 @@
+drop table if exists users;
+create table users (
+  id integer primary key autoincrement,
+  full_name text CHECK( LENGTH(full_name) <= 200 ) not null,
+  small_name text CHECK( LENGTH(small_name) <= 100 ) not null,
+  role text not null,
+  access_level text not null,
+  last_access_date date,
+  last_access_hour time
+);
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='rm79684',
+    packages=['rm79684'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+    setup_requires=[
+        'pytest-runner',
+    ],
+    tests_require=[
+        'pytest',
+    ],
+)
